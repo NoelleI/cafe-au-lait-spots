ee2c57aa66361cd78dd1b650c276cb9fede9be39
Takis Zourntos <takis.zourntos@emads.org>
2020-05-05 13:31:54 -0400

    revised bash script

--- dupimgchk.sh
+++ dupimgchk.sh
@@ -10,20 +10,6 @@ search_path="." # directory which contains the files
 imgfileprefix="image_" # all image files begin with this string
 fileset=$(ls $search_path/$imgfileprefix*) # complete set of files
 
-# create a temporary index file
-temp_indx_filename="temporaneous227.dat"
-if [ -e $temp_indx_filename ]; then
-	rm $temp_indx_filename # remove the temporary file if it already exists
-fi
-indx=0  # our index variable
-for x in $fileset
-do
-	let indx=indx+1	
-	echo "$indx $x" >> $temp_indx_filename # create a line in the temporary file
-done
-number_of_files=$indx
-declare -i number_of_files
-
 # check for diffs amongst all files
 results_file="result_image_comparison.txt"; echo "The following files are identical:" > $results_file
 for x in $fileset
